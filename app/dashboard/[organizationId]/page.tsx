f8765873ca02a1b51d831244092baadc42eb5964
krisna <krisna.w2010@gmail.com>
2025-09-24 13:06:40 +0800

    add shadcn

--- app/dashboard/[organizationId]/page.tsx
+++ app/dashboard/[organizationId]/page.tsx
@@ -1,8 +1,9 @@
 export default async function Page() {
+   const res = process.env.RAILWAY_PUBLIC_DOMAIN;
    return (
        <div>
           Hey welcome to the Dashboard of your organization
-
+          {res}
        </div>
    )
 }
