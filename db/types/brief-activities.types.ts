c57eaca62730db254a98347b2cc1e4e937e9c085
krisna <krisna.w2010@gmail.com>
2025-10-10 13:36:09 +0800

    fixed typography

--- db/types/brief-activities.types.ts
+++ db/types/brief-activities.types.ts
@@ -1,7 +1,3 @@
-// table.query.ts
-// table.schema.ts
-// table.types.ts
-
 import type {InferSelectModel} from "drizzle-orm";
 import {user} from "@/db/schema/auth-schema";
 import {briefActivities} from "@/db/schema/brief-activities.schema";
